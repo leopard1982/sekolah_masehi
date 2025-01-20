b9e2702baa549f43610c72048e989f391e66bdc2
adhy.chandra <adhy.chandra@gmail.com>
2025-01-20 11:10:51 +0700

    menambah blogs materi secara manual

--- fe/urls.py
+++ fe/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import welcome, testPage, tentangKami,blogsSection
+from .views import welcome, testPage, tentangKami,blogsSection,blogsList
 
 urlpatterns = [
     path('', welcome, name='welcome'),
     path('test/',testPage,name='test_page'),
     path('kami/',tentangKami,name='tentang_kami'),
-    path('blogs/<str:id>/',blogsSection,name="blogs_section")
+    path('blogs/<str:id>/',blogsSection,name="blogs_section"),
+    path('blogs/',blogsList,name='blogs_list')
 ]
--- fe/views.py
+++ fe/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse, HttpResponsePermanentRedirect
+import os
+from django.conf import settings
 
 def welcome(request):
     return render(request,'welcome.html')
@@ -16,3 +18,26 @@ def blogsSection(request,id):
         return render(request,'blogs/html1.html')
     else:
         return render(request,'section/kami.html')
+    
+def blogsList(request):
+    lokasi_base_path = str(settings.BASE_DIR)
+    lokasi_template = '\\templates\\blogs\\'
+    gabungan_lokasi = lokasi_base_path+lokasi_template
+    
+    #simpan daftar template ke dalam list []
+    list_template = os.listdir(gabungan_lokasi)
+    
+    #simpan list hanya untuk filename tanpa extension ke dalam list[]
+    list_data = []
+    for list in list_template:
+        #simpan nama file, hilangkan extension dan dapatkan data split di indeks ke 0
+        filename = list.split('.')[0]
+        #tambahkan ke list tanpa ada extension (ditambahkan nanti di template pakai context)
+        list_data.append(filename)
+
+    print(list_data)
+    context = {
+        'list_data':list_data
+    }
+
+    return render(request,'section/blogs.html',context)
