28489d5b83b6e775f2084c7b72bb42898ccb734d
adhy.chandra <adhy.chandra@gmail.com>
2025-01-20 08:40:07 +0700

    tambahkan blogs

--- fe/urls.py
+++ fe/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import welcome, testPage
+from .views import welcome, testPage, tentangKami,blogsSection
 
 urlpatterns = [
     path('', welcome, name='welcome'),
-    path('test/',testPage,name='test_page')
+    path('test/',testPage,name='test_page'),
+    path('kami/',tentangKami,name='tentang_kami'),
+    path('blogs/<str:id>/',blogsSection,name="blogs_section")
 ]
--- fe/views.py
+++ fe/views.py
@@ -7,3 +7,12 @@ def welcome(request):
 def testPage(request):
     return render(request,'test.html')
     # return HttpResponse('Hallo Selamat Pagi')
+
+def tentangKami(request):
+    return render(request,'section/kami.html')
+
+def blogsSection(request,id):
+    if id=="1":
+        return render(request,'blogs/html1.html')
+    else:
+        return render(request,'section/kami.html')
